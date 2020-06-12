1bb51edb4d8f2ff4c0099c2be369e18337ff1c3e
amudia <amudia.ktaruna@gmail.com>
2020-06-12 21:48:59 +0700

    add logout API & fixing get list user

--- src/route/user.js
+++ src/route/user.js
@@ -46,6 +46,22 @@ router.post('/login',(req,res)=>{
 })
 
 
+/* LOGOUT */
+router.get('/logout', auth,(req, res)=>{
+    const token = req.headers.auth_token
+    // const is_revoked = 1
+    const sql = 'UPDATE revoked_token SET is_revoked=1 where token=?'
+    mysql.execute(sql, [ token], (err, result, field)=>{
+        res.send({
+            result,
+            success:true,
+            msg:req.headers.auth_token
+        })
+
+    })
+})
+
+
 /* GET LIST USER */
 router.get('/:id',auth,superadmin,(req, res)=>{
     const {id_role} = req.headers
@@ -63,24 +79,34 @@ router.get('/:id',auth,superadmin,(req, res)=>{
             })
         }
         else if(id_role == 3){
-            const sql = `SELECT tbl_role.id_role, tbl_role.name_role, tbl_tenant.id_tenant, tbl_tenant.name_tenant, tbl_user.id_user, tbl_user.fullname, tbl_user.created_on, tbl_user.updated_on FROM tbl_user INNER JOIN tbl_role ON tbl_user.id_role=tbl_role.id_role INNER JOIN tbl_tenant ON tbl_user.id_tenant=tbl_tenant.id_tenant WHERE tbl_user.id_tenant =1 && tbl_user.id_role BETWEEN 3 AND 5`
+            const sql = `SELECT tbl_role.id_role, tbl_role.name_role, tbl_tenant.id_tenant, tbl_tenant.name_tenant, tbl_user.id_user, tbl_user.fullname, tbl_user.created_on, tbl_user.updated_on FROM tbl_user INNER JOIN tbl_role ON tbl_user.id_role=tbl_role.id_role INNER JOIN tbl_tenant ON tbl_user.id_tenant=tbl_tenant.id_tenant WHERE tbl_user.id_tenant =? && tbl_user.id_role BETWEEN 3 AND 5`
             mysql.execute(sql,[id],(err1,result1,field1)=>{
                 res.send({success:true,data:result1})
             })
         }
         else if(id_role == 4){
-            const sql = `SELECT tbl_role.id_role, tbl_role.name_role, tbl_tenant.id_tenant, tbl_tenant.name_tenant, tbl_user.id_user, tbl_user.fullname, tbl_user.created_on, tbl_user.updated_on FROM tbl_user INNER JOIN tbl_role ON tbl_user.id_role=tbl_role.id_role INNER JOIN tbl_tenant ON tbl_user.id_tenant=tbl_tenant.id_tenant WHERE tbl_user.id_tenant =1 && tbl_user.id_role BETWEEN 4 AND 5`
+            const sql = `SELECT tbl_role.id_role, tbl_role.name_role, tbl_tenant.id_tenant, tbl_tenant.name_tenant, tbl_user.id_user, tbl_user.fullname, tbl_user.created_on, tbl_user.updated_on FROM tbl_user INNER JOIN tbl_role ON tbl_user.id_role=tbl_role.id_role INNER JOIN tbl_tenant ON tbl_user.id_tenant=tbl_tenant.id_tenant WHERE tbl_user.id_tenant =? && tbl_user.id_role BETWEEN 4 AND 5`
             mysql.execute(sql,[id],(err1,result1,field1)=>{
                 res.send({success:true,data:result1})
             })
         }
         else if (id_role == 5){
-            const sql = `SELECT tbl_role.id_role, tbl_role.name_role, tbl_tenant.id_tenant, tbl_tenant.name_tenant, tbl_user.id_user, tbl_user.fullname, tbl_user.created_on, tbl_user.updated_on FROM tbl_user INNER JOIN tbl_role ON tbl_user.id_role=tbl_role.id_role INNER JOIN tbl_tenant ON tbl_user.id_tenant=tbl_tenant.id_tenant WHERE tbl_user.id_tenant =1 && tbl_user.id_role = 5`
+            const sql = `SELECT tbl_role.id_role, tbl_role.name_role, tbl_tenant.id_tenant, tbl_tenant.name_tenant, tbl_user.id_user, tbl_user.fullname, tbl_user.created_on, tbl_user.updated_on FROM tbl_user INNER JOIN tbl_role ON tbl_user.id_role=tbl_role.id_role INNER JOIN tbl_tenant ON tbl_user.id_tenant=tbl_tenant.id_tenant WHERE tbl_user.id_tenant =? && tbl_user.id_role = 5`
             mysql.execute(sql,[id],(err1,result1,field1)=>{
                 res.send({success:true,data:result1})
             })
         }
         
+    })
+ 
+    /* GET DETAIL USER */
+    router.get('/profile/:iduser',auth,(req, res)=>{
+        const {iduser} = req.params
+        const sql = `SELECT tbl_role.id_role, tbl_role.name_role, tbl_tenant.id_tenant, tbl_tenant.name_tenant, tbl_user.id_user, tbl_user.fullname, tbl_user.created_on, tbl_user.updated_on FROM tbl_user INNER JOIN tbl_role ON tbl_user.id_role=tbl_role.id_role INNER JOIN tbl_tenant ON tbl_user.id_tenant=tbl_tenant.id_tenant WHERE tbl_user.id_user=?`
+        mysql.execute(sql,[iduser],(err,result, field)=>{
+            console.log(err)
+            res.send({success:true,data:result})
+        })
     })
     
 /* ADD USER */
@@ -106,13 +132,13 @@ router.post('/',auth,superadmin,(req,res)=>{
 })
 
 /* UPDATE USER */
-router.put('/:id',auth,(req,res)=>{
+router.put('/:id',auth,superadmin,(req,res)=>{
     const {id_tenant, fullname,username,password} =req.body
     const {id} = req.params
     const {id_role}=req.headers
     const enc_pass = bcrypt.hashSync(password)
     const updated_on = new Date()
-    if(id_role == "1") {
+    if(id_role == 1) {
         const sql = `SELECT username from tbl_user WHERE username =?`
         mysql.execute(sql, [username], (err,result1, field)=>{
             if(result1 == ''){
