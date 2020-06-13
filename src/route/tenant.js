22d40fef8de63bf28f7f59dd613cbbb19c872268
amudia <amudia.ktaruna@gmail.com>
2020-06-13 23:47:03 +0700

    add API tenant service & fixing user services

--- src/route/tenant.js
+++ src/route/tenant.js
@@ -14,5 +14,48 @@ const {auth} = require('../middleware')
             res.send({success:true,data:result})
         })
     })
+    /* GET DETAIL TENANT */
+    router.get('/:id',auth,(req, res)=>{
+        const {id} = req.params
+        const sql = `SELECT * FROM tbl_tenant WHERE id_tenant=?`
+        mysql.execute(sql,[id],(err,result, field)=>{
+            console.log(err)
+            res.send({success:true,data:result})
+        })
+    })
+    /*ADD DATA*/
+    router.post('/',auth,(req,res)=>{
+        const {name_tenant} =req.body
+        const created_on = new Date()
+        const updated_on = new Date()
+        const sql = 'INSERT INTO tbl_tenant (name_tenant, created_on,updated_on) VALUES (?,?,?)'
+        mysql.execute(sql,
+            [name_tenant,created_on,updated_on],
+            (err,result,field)=>{
+            res.send({success:true,data:result})
+        })
+    })
+    /*EDIT DATA */
+    router.put('/:id',auth,(req,res)=>{
+    const {name_tenant} = req.body
+    const {id} = req.params
+    const updated_on = new Date()
+    const sql =`UPDATE tbl_tenant SET name_tenant=?, updated_on=? WHERE id_tenant=?`
+    mysql.execute(sql,
+        [name_tenant,updated_on,id],
+        (err,result,field)=>{
+            res.send({succes:true,data:result})
+        })
+    })
+
+    /*DELETE DATA */
+    router.delete('/:id',auth,(req,res)=>{
+    const {id} = req.params
+    const sql = `DELETE FROM tbl_tenant where id_tenant=?`
+    mysql.execute(sql,
+        [id],(err,result,field)=>{
+            res.send({succes:true,data:result})
+        })
+})
 
 module.exports =router
